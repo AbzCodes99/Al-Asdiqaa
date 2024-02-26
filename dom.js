ca15abd7d73adaeca6d8b0b0b10b36114efc496b
Abdi Ali <abdiali@Abdis-Air.broadband>
2024-02-26 01:20:13 +0000

    trying to fix profile js

---
+++ dom.js
@@ -0,0 +1,12 @@
+const body = document.body
+const profile = document.getElementsByClassName("mainProfile")
+const email = document.getElementsByClassName("email")
+const password = document.getElementsByClassName("password")
+const btn = document.getElementsByClassName("button")
+
+profile.addEventListener("submit",btnSign)
+
+
+function btnSign(){
+    alert('hey')
+}
